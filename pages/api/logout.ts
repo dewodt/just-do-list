120ac1cc15144d9743d9b77b1dc3ce4fc3ca0cdd
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2023-01-11 16:20:52 +0700

    Changed cookie name to username of account (To retrieve usename data)

--- pages/api/logout.ts
+++ pages/api/logout.ts
@@ -4,6 +4,7 @@ export default async function logOut(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  res.setHeader('Set-Cookie', 'Login-JustDoList=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT');
+  const { username } = req.body;
+  res.setHeader('Set-Cookie', `${username}=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`);
   res.json({})
 }
